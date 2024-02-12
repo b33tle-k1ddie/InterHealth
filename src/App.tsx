f0a01047bf0d85a9cb8ce8965eeda9dd37f9adda
b33tle-k1ddie <d4rks1d3r3dbull@protonmail.com>
2024-02-12 13:25:02 +0200

    add Apollo Client + UI gen page

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ import { IonReactRouter } from '@ionic/react-router';
 import Home from './pages/Home';
 import SearchPage from './pages/Search-page';
 import PhotoPage from './pages/Photo-page';
+import CountryPage from './pages/Country-page'
 
 /* Core CSS required for Ionic components to work properly */
 import '@ionic/react/css/core.css';
@@ -39,6 +40,7 @@ const App: React.FC = () => (
         </Route>
         <Route path="/search" component={SearchPage} exact={true} />
         <Route path="/photo" component={PhotoPage} exact={true} />
+        <Route path="/country" component={CountryPage} exact={true} />
       </IonRouterOutlet>
     </IonReactRouter>
   </IonApp>
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,25 +1,109 @@
-// Home.tsx
 import React from 'react';
-import { IonButton, IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/react';
+import { IonButton, IonHeader, IonToolbar, IonTitle, IonContent, IonImg, IonCard, IonCardContent, IonCardHeader, IonCardTitle, IonTabs, IonTabBar, IonTabButton, IonIcon, IonLabel, IonRouterOutlet, IonPage } from '@ionic/react';
+import { home, globe } from 'ionicons/icons';
 import { Link } from 'react-router-dom';
+import { IonReactRouter } from '@ionic/react-router';
+import { Route, Redirect } from 'react-router';
+import CountryPage from '../pages/Country-page';
+import "./Home.css";
+import { useQuery, ApolloProvider, ApolloClient, InMemoryCache, gql } from '@apollo/client';
+const Tables: React.FC = ()=>{
+  const GET_ALL_USERS = gql`
+  query {
+    getAllUsers {
+      id
+      username
+      age
+    }
+  }
+`;
+
+const { data, loading, error } = useQuery(GET_ALL_USERS);
+
+if (loading) return <p>Loading...</p>;
+
+if (error) {
+  console.error('Error fetching data:', error);
+  return <p>Error: {error.message}</p>;
+}
+console.log(data);
+}
 
 const Home: React.FC = () => (
-  <>
+  <IonPage>
     <IonHeader>
       <IonToolbar>
-        <IonTitle>InterHealth</IonTitle>
+        <IonImg src="../src/assets/media/banner.png" id="" />
       </IonToolbar>
     </IonHeader>
+
     <IonContent className="ion-padding">
-      <h1>InterHealth App by DPSU_IT_HUB</h1>
-      <Link to="/search">
-        <IonButton routerDirection="forward">Go to Search Page</IonButton>
-      </Link>
-      <Link to="/photo">
-        <IonButton routerDirection="forward">Go to Photo Page</IonButton>
-      </Link>
+      <IonCard color="success">
+        <IonCardHeader>
+          <IonCardTitle>FIND DRUGS</IonCardTitle>
+        </IonCardHeader>
+        <IonCardContent>
+          <IonImg src="../src/assets/media/med.png" id="card-photo" />
+          <Link to="/search">
+            <IonButton routerDirection="forward">Go to Search Page</IonButton>
+          </Link>
+        </IonCardContent>
+      </IonCard>
+
+      <IonCard color="warning">
+        <IonCardHeader>
+          <IonCardTitle>TAKE A PICTURE</IonCardTitle>
+        </IonCardHeader>
+        <IonCardContent>
+          <IonImg src="../src/assets/media/pic.png" id="card-photo" />
+          <Link to="/photo">
+            <IonButton routerDirection="forward">Go to Photo Page</IonButton>
+          </Link>
+        </IonCardContent>
+      </IonCard>
     </IonContent>
-  </>
+  </IonPage>
+  );
+
+  
+
+const AppTabs: React.FC = () => (
+  <IonTabs>
+    <IonRouterOutlet>
+      <Route path="/home" render={() => <Home />} exact={true} />
+      <Route path="/country" render={() => <CountryPage />} exact={true} />
+      <Redirect exact path="/" to="/home" />
+    </IonRouterOutlet>
+
+    <IonTabBar slot="bottom">
+      <IonTabButton tab="home" href="/home">
+        <IonIcon icon={home} />
+        <IonLabel>General</IonLabel>
+      </IonTabButton>
+
+      <IonTabButton tab="country" href="/country">
+        <IonIcon icon={globe} />
+        <IonLabel>Country</IonLabel>
+      </IonTabButton>
+    </IonTabBar>
+  </IonTabs>
 );
 
-export default Home;
+const App: React.FC = () => (
+  
+  <ApolloProvider client={client}>
+  <IonReactRouter>
+    <Home />
+    <AppTabs />
+    <Tables />
+  </IonReactRouter>
+  </ApolloProvider>
+);
+const client = new ApolloClient({
+  uri: 'http://172.20.10.5:5000/graphql',
+  cache: new InMemoryCache(),
+});
+
+
+
+export default App;
